4bd97e083f22b9260018a267b70cdecb023f113e
Olivia <oliviad3955@tywls.org>
2019-05-08 22:09:20 +0000

    shae

--- js/script.js
+++ js/script.js
@@ -1,12 +1,28 @@
 /* global $ */
+
+
 $(function () {
     $("#Search").click(function(){
+         var searchTerm = $("#searchTerm").val();
+        
+        
         $.ajax({
-            url: "https://api.openweathermap.org/data/2.5/forecast/hourly?zip=94040&APPID=9f28e0be94c5f8477546f61945dbb3e0",
+            url: "https://api.openweathermap.org/data/2.5/forecast?q="+searchTerm+",us&appid=9f28e0be94c5f8477546f61945dbb3e0",
+            
             method: "GET",
             success: function(response) {
-                // code
                 console.log(response);
+                // $("body").append(response.city.name);
+                
+                $.each(response.list, function(index, value){
+                    console.log(value);
+                    
+                var time = value.dt_txt    
+                $(".data").append(time); 
+                var degree = value.wind.deg;
+                });
+                // code
+                
             }
         });
     });
